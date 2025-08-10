be67ebea5b4d7f738cd8734149189be147ce31bb
ochkarik12 <pere12@yandex.ru>
2025-08-10 19:33:27 +0300

    Разбираемся с циклом событий Node js

--- node/01-event-loop-events/index.js
+++ node/01-event-loop-events/index.js
@@ -1,4 +1,6 @@
 const fs = require('fs')
+const dns = require('dns')
+const { time, timeStamp } = require('console')
 
 function timestamp(){
   return performance.now().toFixed(2)
@@ -7,21 +9,36 @@ function timestamp(){
 
 console.log('Program start', timestamp())
 
+
+
+//Close events
+fs.writeFile('./test.txt', 'Hello Node.js', () => console.log('File written', timestamp()))
+
+//Promises
+Promise.resolve().then(() => console.log('Promise 1', timestamp()))
+
+//Next tick
+process.nextTick(() => console.log('nextTick 1', timestamp()))
+
+//Set immidiate (Check)
+setImmediate(() => console.log('Imidiate 1', timestamp()))
+
+//Timeouts
 setTimeout(() => {
   console.log('Timeout 1', timestamp())
 }, 0)
 
 setTimeout(() => {
+  process.nextTick(() => console.log('nextStick 2', timestamp()))
+
   console.log('Timeout 2', timestamp())
 }, 10)
 
-fs.writeFile('./test.txt', 'Hello Node.js', () => console.log('File written', timestamp()))
-
-
-Promise.resolve().then(() => console.log('Promise 1', timestamp()))
-
-process.nextTick(() => console.log('nextTick 1', timestamp()))
-
-setImmediate(() => console.log('Imidiate 1', timestamp()))
+// I/O Events
+dns.lookup('localhost', (err, address, family) => {
+  console.log('DNS 1 localhost', address, timestamp())
+  Promise.resolve().then(() => console.log ('Promise 2', timestamp()))
+  process.nextTick(() => console.log('nextTick 3', timestamp()))
+})
 
 console.log('Program end', timestamp())
